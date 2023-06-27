95530c3f145cc0bc76f77566f7cba6b99e9c747f
marcelofaraj <marcelofaraj@gmail.com>
2023-06-27 14:44:02 +0200

    modules removed

--- experimental_data/Local Motif Clustering via (Hyper)Graph Partitioning/full_algorithms/extern/KaMinPar/kaminpar/initial_partitioning/bfs_bipartitioner.cc
+++
@@ -1,9 +0,0 @@
-#include "initial_partitioning/bfs_bipartitioner.h"
-
-namespace kaminpar {
-template class bfs::BfsBipartitioner<bfs::alternating>;
-template class bfs::BfsBipartitioner<bfs::lighter>;
-template class bfs::BfsBipartitioner<bfs::sequential>;
-template class bfs::BfsBipartitioner<bfs::longer_queue>;
-template class bfs::BfsBipartitioner<bfs::shorter_queue>;
-} // namespace kaminpar
